65220a5ef17341153ae0bda20bdbcd6fe5dd2087
Mubashir Ahmed <mdmubashirahmed12345@gmail.com>
2023-11-02 11:21:51 +0530

    Let's deploy

--- index.py
+++ index.py
@@ -27,22 +27,22 @@ def submit():
         choices = request.form['choices']
         difficulty = request.form['difficulty']
 
-        # response = openai.ChatCompletion.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=[
-        #         {
-        #             "role": "system",
-        #             "content": f"Hey chat gpt prepare a quick quiz on this topic: {topic} and prepare {questions} number of questions and for each of them keep {choices} number of choices, also keep the difficulty level {difficulty}, reply in the form of an object, make sure the response object contains topic, questions array containing question, choices and it's answer"
-        #         }
-        #     ],
-        #     temperature=0.7
-        # )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"Hey chat gpt prepare a quick quiz on this topic: {topic} and prepare {questions} number of questions and for each of them keep {choices} number of choices, also keep the difficulty level {difficulty}, reply in the form of an object, make sure the response object contains topic, questions array containing question, choices and it's answer"
+                }
+            ],
+            temperature=0.7
+        )
 
-        # quiz_content = response["choices"][0]["message"]["content"]
-        # print(quiz_content)
-        # response = json.loads(quiz_content)
+        quiz_content = response["choices"][0]["message"]["content"]
+        print(quiz_content)
+        response = json.loads(quiz_content)
 
-        response = {'topic': 'Computer Networks', 'questions': [{'question': 'Which protocol is used for email communication?', 'choices': ['SMTP', 'HTTP', 'FTP'], 'answer': 'SMTP'}, {'question': 'What is the purpose of a firewall in a computer network?', 'choices': ['To block viruses', 'To prevent unauthorized access', 'To increase network speed'], 'answer': 'To prevent unauthorized access'}]}
+        # response = {'topic': 'Computer Networks', 'questions': [{'question': 'Which protocol is used for email communication?', 'choices': ['SMTP', 'HTTP', 'FTP'], 'answer': 'SMTP'}, {'question': 'What is the purpose of a firewall in a computer network?', 'choices': ['To block viruses', 'To prevent unauthorized access', 'To increase network speed'], 'answer': 'To prevent unauthorized access'}]}
         session['response'] = response
         # print(response)
         return render_template("quiz.html", content=response)
@@ -65,4 +65,4 @@ def submit():
         return render_template("score.html", score=score, correct_answers=actual_answers, given_answers=given_answers)
 
 
-app.run(debug=True)
+
